78686d2f29ce1c8c655d90c8272ed3d2e7fd5613
Luciana Santos <luguys@gmail.com>
2022-04-20 22:59:00 -0300

    update

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,78 +1,76 @@
 // Função de declaração do programa(tudo tem que estar dentro );
-var calculadora = () => {
-
+const calculadora = () => {
   // Declarações das entradas de informações que o usuário digita no teclado. Não esquecer quando o valor é númerico. Tem que declar o tipo NUMBER para o uso do (prompt()..); entre parenteses;
-  const operacao = Number(
+  let operacao = Number(
     prompt(
-      'Escolha a operação:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão (/)\n 5 - divisaoInteira (%)\n 6 - potenciação (**)',
+      'Escolha a operação:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão (/)\n 5 - divisao_Inteiros (%)\n 6 - potenciação (**)',
     ),
   );
 
   // Esssa condição verifica e bloqueia digitações inválidas da CONST-operacao. Caso seja verdadeira ela executa a condição; Se for falsa a calculadora continua sendo executada normalmente;
   if (!operacao || operacao >= 7) {
-    alert('Operação inválida!')
+    alert('Operação inválida!');
     calculadora();
-
-  } else {
-
+  }else {
     //Capturando os valores para os cálculos inseridos pelo usuário
     let n1 = Number(prompt('Insira o 1º valor'));
     let n2 = Number(prompt('Insira o 2º valor'));
     let resultado;
 
     // Funções dos cálculos matématicos
-    var soma = () => {
+    let soma = () => {
       resultado = n1 + n2;
       alert(`${n1} + ${n2} = ${resultado}`);
       novaOp();
     };
 
-    var subtração = () => {
+    let subtração = () => {
       resultado = n1 - n2;
       alert(`${n1} - ${n2} = ${resultado}`);
       novaOp();
     };
 
-    var multiplicação = () => {
+    let multiplicação = () => {
       resultado = n1 * n2;
       alert(`${n1} * ${n2} = ${resultado}`);
       novaOp();
     };
 
-    var divisão = () => {
+    let divisão = () => {
       resultado = n1 / n2;
       alert(`${n1} / ${n2} = ${resultado}`);
       novaOp();
     };
 
-    var divisaoInteira = () => {
+    let divisaoInteira = () => {
       resultado = n1 % n2;
       alert(`O resto da divisão entre ${n1} e  ${n2} é igual a ${resultado}`);
       novaOp();
     };
 
-    var potenciação = () => {
+    let potenciação = () => {
       resultado = n1 ** n2;
       alert(` ${n1} elevado a ${n2}ª é igual a ${resultado}`);
       novaOp();
     };
 
-
+ 
+  }
 
     //Pergunta ao user se quer continuar calculando ou sair do programa após uma execução de cálculo
-    var novaOp = () => {
+    let novaOp = () => {
       let opcao = prompt(
         `Deseja realizar outra operação?\n 1 - SIM \n 2 - NÃO`,
       );
 
-       if (opcao == 1) {
+      if (opcao == 1) {
         calculadora();
       } else if (opcao == 2) {
-        alert('Até a próxima!');
+        alert('Até a próxima! ;)');
       } else {
         alert('Digite uma opção válida!');
         novaOp();
-      }  
+      }
 
       /* Condição direta para saída do programa 
 
@@ -109,6 +107,6 @@ var calculadora = () => {
         potenciação();
         break;
     }
-  }
-};
+  };
+
 calculadora();
